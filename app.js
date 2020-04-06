c8e06669411b1edfaaaf3bb903ef0dd20f7767b5
aakashsr <aakashsri.dev@gmail.com>
2020-04-06 20:37:41 +0530

    executed addItem method inside controller module with actual input type,des and val which was returned by UIController module inside controller module

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ var budgetController = (function () {
       var newItem, ID;
 
       // Create new ID
-      ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
 
       // Create new item based on 'inc' or 'exp' type
       if (type === "exp") {
@@ -37,6 +41,9 @@ var budgetController = (function () {
 
       // Return the new element
       return newItem;
+    },
+    testing: function () {
+      console.log(data);
     },
   };
 })();
@@ -82,6 +89,11 @@ var controller = (function (budgetCtrl, UICtrl) {
     // 1. Get the field input data
     var input = UIController.getInput();
     // 2. Add the item to the budget controller
+    var newItem = budgetController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
@@ -94,6 +106,6 @@ var controller = (function (budgetCtrl, UICtrl) {
       setupEventListeners();
     },
   };
-})();
+})(budgetController, UIController);
 
 controller.init();
